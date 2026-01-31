795031b6b51dfececb6dbefc06a34f8cf97a166b
Teapot418 <marmtk1811@gmail.com>
2026-01-31 14:11:26 -0500

    Refactor and add vision code.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,84 +5,71 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.StartEndCommand;
-
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
-import com.pathplanner.lib.commands.PathPlannerAuto;
-
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.Joystick;
 
 import frc.robot.subsystems.Swerve;
 
 public class RobotContainer {
-    
 
-private final XboxController controller = new XboxController(0);
-private final Joystick operator = new Joystick(1);
+    private final XboxController controller = new XboxController(0);
+    private final Joystick operator = new Joystick(1);
 
-private final int translationAxis = XboxController.Axis.kLeftY.value;
-private final int strafeAxis = XboxController.Axis.kLeftX.value;
-private final int rotationAxis = XboxController.Axis.kRightX.value;
+    private final int translationAxis = XboxController.Axis.kLeftY.value;
+    private final int strafeAxis = XboxController.Axis.kLeftX.value;
+    private final int rotationAxis = XboxController.Axis.kRightX.value;
 
-private final JoystickButton zeroGyro = new JoystickButton(controller, XboxController.Button.kY.value);
+    private final JoystickButton zeroGyro = new JoystickButton(controller, XboxController.Button.kY.value);
 
-private final SendableChooser<Command> autoChooser;
+    private final SendableChooser<Command> autoChooser;
 
-/*Subsystems */
-private final Swerve s_swerve = new Swerve();
+    /* Subsystems */
+    private final Swerve s_swerve = new Swerve();
 
+    public RobotContainer() {
 
-public RobotContainer() {
+        /*
+         * s_swerve.setDefaultCommand(new RunCommand(
+         * () -> s_swerve.drive(
+         * -MathUtil.applyDeadband(controller.getLeftX(), Constants.stickDeadband),
+         * -MathUtil.applyDeadband(controller.getLeftY(), Constants.stickDeadband),
+         * -MathUtil.applyDeadband(controller.getRightX(), Constants.stickDeadband),
+         * true),
+         * s_swerve));
+         * 
+         */
+        configureDriverControls();
 
-    /* 
-    s_swerve.setDefaultCommand(new RunCommand(
-        () -> s_swerve.drive(
-            -MathUtil.applyDeadband(controller.getLeftX(), Constants.stickDeadband),
-            -MathUtil.applyDeadband(controller.getLeftY(), Constants.stickDeadband),
-            -MathUtil.applyDeadband(controller.getRightX(), Constants.stickDeadband),
-            true),
-        s_swerve));
-    
-    */
-    configureDriverControls();
-    
-    s_swerve.setDefaultCommand(new RunCommand(
-        () -> s_swerve.drive(
-            controller.getRawAxis(translationAxis),
-            controller.getRawAxis(strafeAxis),
-            controller.getRawAxis(rotationAxis),
-            true),
-        s_swerve));
+        s_swerve.setDefaultCommand(new RunCommand(
+                () -> s_swerve.drive(
+                        controller.getRawAxis(translationAxis),
+                        controller.getRawAxis(strafeAxis),
+                        controller.getRawAxis(rotationAxis),
+                        true),
+                s_swerve));
 
-    NamedCommands.registerCommand("zeroGyro", new InstantCommand(() -> s_swerve.zeroGyro()));
+        NamedCommands.registerCommand("zeroGyro", new InstantCommand(() -> s_swerve.zeroGyro()));
 
-    autoChooser = AutoBuilder.buildAutoChooser();
-    SmartDashboard.putData("Auto Chooser", autoChooser);
+        autoChooser = AutoBuilder.buildAutoChooser();
+        SmartDashboard.putData("Auto Chooser", autoChooser);
 
-    configureButtonBindings();
-    
-        
+        configureButtonBindings();
 
-/*Subsystems */
-
-
+        /* Subsystems */
 
         // Configure the button bindings ( what a useful comment)
-    
-  //  configureOperatorControls();
-}
 
-public Command getAutonomousCommand(){
-    return autoChooser.getSelected();
-}
+        // configureOperatorControls();
+    }
 
-private void configureDriverControls() {
+    public Command getAutonomousCommand() {
+        return autoChooser.getSelected();
+    }
+
+    private void configureDriverControls() {
 
         /* Drive Controls */
         // this code sets up driving in teleop.
@@ -93,11 +80,9 @@ private void configureDriverControls() {
 
         JoystickButton zeroGyro = new JoystickButton(controller, XboxController.Button.kY.value);
 
+    }
 
-
-}
-
-private void configureButtonBindings() {
-    zeroGyro.onTrue(new InstantCommand(() -> s_swerve.zeroHeading()));
-}
+    private void configureButtonBindings() {
+        zeroGyro.onTrue(new InstantCommand(() -> s_swerve.zeroHeading()));
+    }
 }
--- src/main/java/frc/robot/subsystems/MAXSwerveModule.java
+++ src/main/java/frc/robot/subsystems/MAXSwerveModule.java
@@ -1,197 +1,172 @@
 package frc.robot.subsystems;
 
-
-import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.ClosedLoopSlot;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkClosedLoopController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-import frc.lib.util.swerveUtil.CTREModuleState;
-
-
 import com.ctre.phoenix6.hardware.CANcoder;
-import com.fasterxml.jackson.databind.type.ResolvedRecursiveType;
-
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
-import frc.lib.math.Conversions;
-
-import frc.robot.Configs;
-
 import frc.robot.Configs;
 import frc.robot.Constants;
+import frc.robot.Constants.Swerve.ModuleConstants;
 
 public class MAXSwerveModule {
+    // Motors
     private SparkMax m_drivingSpark;
     private SparkMax m_turningSpark;
 
+    // Motor Encoders
     private RelativeEncoder m_drivingEncoder;
     private RelativeEncoder m_turningEncoder;
 
+    // Configuration object containing motor IDs, offsets, etc.
+    private final ModuleConstants moduleConstants;
+
     private final SparkClosedLoopController m_drivingClosedLoopController;
     private final SparkClosedLoopController m_turningClosedLoopController;
-    
-    private CANcoder angleEncoder;
 
+    private CANcoder angleEncoder;
     private double m_chassisAngularOffset = 0;
     private SwerveModuleState m_desiredState = new SwerveModuleState(0.0, new Rotation2d());
-
     private double rotval = 0;
 
-    public MAXSwerveModule(int drivingCANId, int turningCANId, int cancoderId, double chassisAngularOffset) {
-        m_drivingSpark = new SparkMax(drivingCANId, MotorType.kBrushless);
-        m_turningSpark = new SparkMax(turningCANId, MotorType.kBrushless);
-        
+    public MAXSwerveModule(ModuleConstants moduleConstants) {
+        // Initialize motors and encoders using constants
+        this.moduleConstants = moduleConstants;
+        m_drivingSpark = new SparkMax(moduleConstants.drivermotorID0, MotorType.kBrushless);
+        m_turningSpark = new SparkMax(moduleConstants.anglemotorID0, MotorType.kBrushless);
+
         m_drivingClosedLoopController = m_drivingSpark.getClosedLoopController();
         m_turningClosedLoopController = m_turningSpark.getClosedLoopController();
 
-        angleEncoder = new CANcoder(cancoderId);
+        angleEncoder = new CANcoder(moduleConstants.camcoderID);
         configEncoders();
 
-        m_turningSpark.configure(Configs.MAXSwerveModule.turningConfig, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
-        m_drivingSpark.configure(Configs.MAXSwerveModule.drivingConfig, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
-    
+        m_turningSpark.configure(Configs.MAXSwerveModule.turningConfig, ResetMode.kNoResetSafeParameters,
+                PersistMode.kPersistParameters);
+        m_drivingSpark.configure(Configs.MAXSwerveModule.drivingConfig, ResetMode.kNoResetSafeParameters,
+                PersistMode.kPersistParameters);
 
+        m_chassisAngularOffset = this.moduleConstants.offset0;
 
-        
-       
-        m_chassisAngularOffset = chassisAngularOffset;
-        
-        
         resetToAbsolute();
 
-        
-        
         m_desiredState.angle = new Rotation2d(m_turningEncoder.getPosition());
     }
 
-    private void configEncoders(){
+    /**
+     * Constants about this module like motor IDs and offsets.
+     */
+    public ModuleConstants getModuleConstants() {
+        return moduleConstants;
+    }
+    private void configEncoders() {
         angleEncoder.getConfigurator().apply(Configs.MAXSwerveModule.swerveCANcoderconfig);
-        
+
         m_drivingEncoder = m_drivingSpark.getEncoder();
         m_drivingEncoder.setPosition(0);
 
         m_turningEncoder = m_turningSpark.getEncoder();
 
-        
     }
-
-    public void resetToAbsolute(){
-        double absolutePosition = getCanCoder().getRadians() - m_chassisAngularOffset;
-        m_turningEncoder.setPosition(absolutePosition); 
+    /**
+     * Sets the turning encoder to the absolute position of the CANCoder minus the
+     * chassis angular offset.
+     */
+    public void resetToAbsolute() {
+        double absolutePosition = getCanCoder().getDegrees() - m_chassisAngularOffset;
+        m_turningEncoder.setPosition(absolutePosition);
     }
-    
-
-    
-    public SwerveModulePosition getPosition() { 
+    /**
+     * 
+     * @return The current position of the swerve module as a SwerveModulePosition.
+     */
+    public SwerveModulePosition getPosition() {
         return new SwerveModulePosition(m_drivingEncoder.getPosition(), getAngle());
     }
-
-    public double getEncoderPosition(){
+    /**
+     * 
+     * @return The current position of the turning encoder in degrees.
+     */
+    public double getEncoderPosition() {
         return m_turningEncoder.getPosition();
     }
-    
-
-    public SwerveModuleState getState(){
+    /**
+     * 
+     * @return The current state of the swerve module as a SwerveModuleState.
+     */
+    public SwerveModuleState getState() {
         return new SwerveModuleState(m_drivingEncoder.getVelocity(), getAngle());
     }
-
+    /**
+     * Sets the desired state of the swerve module.
+     * 
+     * @param desiredState The desired state to set.
+     */
     public void setDesiredState(SwerveModuleState desiredState) {
-        /*
-        SmartDashboard.putNumber("Desired Angle Check 4", desiredState.angle.getDegrees());
-        desiredState = CTREModuleState.optimize(desiredState, getState().angle);
-        
-        SmartDashboard.putNumber("Desired Angle Check 3", desiredState.angle.getDegrees());
-        
-        
-        SwerveModuleState correctedDesiredState = new SwerveModuleState();
-        correctedDesiredState.speedMetersPerSecond = desiredState.speedMetersPerSecond;
-        correctedDesiredState.angle = desiredState.angle.plus(Rotation2d.fromDegrees(m_chassisAngularOffset));
-        SmartDashboard.putNumber("Desired Angle Check 2", correctedDesiredState.angle.getDegrees());
-        correctedDesiredState = CTREModuleState.optimize(correctedDesiredState, getState().angle);
-        
-        SmartDashboard.putNumber("Desired Angle Check 1", correctedDesiredState.angle.getDegrees());
-        
-
-        */
-
-
-        SmartDashboard.putNumber("Original Desired Angle", desiredState.angle.getDegrees());
         SwerveModuleState correctedDesiredState = new SwerveModuleState();
         correctedDesiredState.speedMetersPerSecond = desiredState.speedMetersPerSecond;
         correctedDesiredState.angle = desiredState.angle;
         correctedDesiredState.optimize(new Rotation2d(m_turningEncoder.getPosition()));
-        SmartDashboard.putNumber("Desired Angle", correctedDesiredState.angle.getDegrees());
 
         setAngle(correctedDesiredState);
         setSpeed(correctedDesiredState, true);
-
-        m_desiredState = desiredState;
-        
     }
-
-    private Rotation2d getAngle(){
+    /**
+     * 
+     * @return The current angle of the swerve module as a Rotation2d.
+     */
+    private Rotation2d getAngle() {
         return Rotation2d.fromDegrees(m_turningEncoder.getPosition());
     }
-
-    public Rotation2d getCanCoder(){
+    /**
+     * 
+     * @return The absolute position of the CANCoder as a Rotation2d.
+     */
+    public Rotation2d getCanCoder() {
         return Rotation2d.fromRotations(angleEncoder.getAbsolutePosition().getValueAsDouble());
     }
+    /**
+     * Sets the angle of the swerve module.
+     * 
+     * @param desiredState The desired state to set the angle to.
+     */
+    private void setAngle(SwerveModuleState desiredState) {
 
-    private void setAngle(SwerveModuleState desiredState)
-    {
-        
-        if(Math.abs(desiredState.speedMetersPerSecond) <= (Constants.kMaxAngularSpeed*0.01))
-       {
-        m_turningSpark.stopMotor();
-        return;
-       }
-        
-         
-        
-        
-        
+        if (Math.abs(desiredState.speedMetersPerSecond) <= (Constants.Swerve.kMaxAngularSpeed * 0.01)) {
+            m_turningSpark.stopMotor();
+            return;
+        }
         rotval = desiredState.angle.getRadians();
-        
-        m_turningClosedLoopController.setReference(rotval, ControlType.kPosition);
-            
-    }
 
-    private void setSpeed(SwerveModuleState desiredState, boolean isOpenLoop)
-    {
-       
-        if(isOpenLoop)
-        {
-            double percentOutput = desiredState.speedMetersPerSecond / Constants.kMaxSpeedMetersPerSecond;
+        m_turningClosedLoopController.setReference(rotval, ControlType.kPosition);
+    }
+    /**
+     * Sets the speed of the swerve module.
+     * 
+     * @param desiredState The desired state to set the speed to.
+     * @param isOpenLoop   Whether to use open-loop control or closed-loop control.
+     */
+    private void setSpeed(SwerveModuleState desiredState, boolean isOpenLoop) {
+
+        if (isOpenLoop) {
+            double percentOutput = desiredState.speedMetersPerSecond / Constants.Swerve.kMaxSpeedMetersPerSecond;
             m_drivingSpark.set(percentOutput);
             return;
         }
- 
-        
-        
-        
-        
         m_drivingClosedLoopController.setReference(desiredState.speedMetersPerSecond, ControlType.kVelocity);
-        
     }
-
-    public double getRotVal(){
+    /**
+     * 
+     * @return The current rotational value setpoint of the swerve module in
+     *         radians.
+     */
+    public double getRotVal() {
         return rotval;
     }
-    
-
-
-
 }
